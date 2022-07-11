a2e645e08ed3148dbd2f91e775ffcaccb5eabf23
Sl1nki <sl1nki@protonmail.com>
2022-07-10 18:18:24 -0600

    Expanded digit length on vpnpanel ping.  Removed autostart for banner.sh

--- opt/vpnpanel.sh
+++ opt/vpnpanel.sh
@@ -5,7 +5,7 @@ lab=$(cat /etc/openvpn/client/*.conf | grep "remote " | cut -d " " -f 2 | cut -d
 if [[ $htbip == *"10."* ]]
 then
    gwip=$(/usr/sbin/route -n | grep tun0 | grep -m 1 UG | tr -s " " | cut -d " " -f 2)
-   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-4)
+   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-5)
    echo "HTB VPN: $lab ($htbip) [$ping ms]"
 else
    echo "HTB VPN: Disconnected"
